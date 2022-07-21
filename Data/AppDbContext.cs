246d1d875c16a4ecb5a668f6e55d71ff7df384ce
Thiyagarajan Ramu <zxtru1@wowcorp.com.au>
2022-07-21 18:44:41 +1000

    First commit with migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using GraphQLDotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLDotnet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms {get; set;}
+    }
+
+}
